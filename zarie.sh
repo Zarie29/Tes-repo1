4fd05e4f62bdac68f9ca3c548d465a6f323668c7
Zarie29 <zarie705@gmail.com>
2025-12-12 03:12:39 +0000

    init test

---
+++ zarie.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+nama="Zarie Ilham"
+nim="241058012"
+
+if [[ "$nama" == "Zarie Ilham" && "$nim" == "241058012" ]]; then
+    echo "Halo Zarie Ilham! NIM cocok!"
+else
+    echo "Data tidak cocok!"
+fi
